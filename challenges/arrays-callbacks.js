ff3ce8037894773dba9ea9b67c3d3ef112207646
Katie Olson <katieolson@katies-air.lan>
2020-10-16 14:10:53 -0600

    Request 1 corrected, arrays-callbacks

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -20,11 +20,12 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = zooAnimals.forEach(item => item.animal_name && item.scientific_name);
 
+const displayNames = [];
+zooAnimals.forEach(function(item){
+ displayNames.push(`Name: ${item.animal_name}, Scientific: ${item.scientific_name}`);
+});
 console.log(displayNames);
-
-// not working
 
 
 /* Request 2: .map()
