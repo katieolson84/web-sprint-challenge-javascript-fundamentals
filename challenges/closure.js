524977acf6daa9dfa5f86c214c5a3e167cdfe325
Katie Olson <katieolson@katies-air.lan>
2020-10-16 11:54:25 -0600

    completed Closure js

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,17 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// Functions keep data within the fucntion 'box'. It protects the data from outside attacks or calls. If data is within the fuction and uses const and let is can only be accessed by calling within the function. When you put variables outside of the function. You are not putting them in the global or external space and they can be accessed when called outside the fucntion. 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num) {
+  let summation = 0;
+  for (let i = 1; i <= num; i++) {
+    summation += i;
+  }
+  return summation;
+}
+console.log( summation(4));
